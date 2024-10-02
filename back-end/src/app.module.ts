e56a4f7a090ea964e740ca9ee51864bb453afe61
ytijani <yousseflolo120@gmail.com>
2024-10-02 23:23:04 +0100

    connect nestjd with mongodb

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        const user = process.env.DATABASE_USER;
+        const password = process.env.DATABASE_PASSWORD;
+        const dbName = process.env.DATABASE_NAME;
+        const host = process.env.DATABASE_HOST || 'localhost';
+        
+        return {
+          uri: `mongodb://${user}:${password}@${host}`,
+        };
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
