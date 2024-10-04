e5473b7b5251d2025dbc6dde60bfe3e97b1fb86c
ytijani <yousseflolo120@gmail.com>
2024-10-04 11:48:11 +0100

    change env variable and add url

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -8,10 +8,8 @@ import { CoursesModule } from './courses/courses.module';
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
       useFactory: () => {
-        const host = process.env.DATABASE_HOST;
-        const dbname = process.env.DATABASE_NAME;
         return {
-          uri: `mongodb://${host}/${dbname}`,
+          uri: process.env.DATABASE_URL,
         };
       },
     }),
